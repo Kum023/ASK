df9efef1fda3ecddf0a5387687d26b83011e2d00
Kumara023 <guru0203kum@gmail.com>
2023-12-26 19:54:02 +0800

    https://github.com/Kum023/ask_online-education.git

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Get the DOM elements for the image carousel
 const wrapper = document.querySelector(".wrapper"),
   carousel = document.querySelector(".carousel"),
   images = document.querySelectorAll("img"),
